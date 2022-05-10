6ba2d9f97d36d2cdaea9b0331ad251f38379e5bc
NicolasFrancolino <87394098+NicolasFrancolino@users.noreply.github.com>
2022-05-10 13:43:36 -0300

    Agregar archivos de proyecto.

---
+++ Challenge6Ejer1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge6Ejer1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangulo rectangulo = new Rectangulo();
+            rectangulo.Lado1 = 4;
+            rectangulo.Lado2 = 8;
+            Console.WriteLine("la superficie del rectangulo es " + rectangulo.RetornarSuperficie());
+            Console.WriteLine("el perimetro del rectangulo es " + rectangulo.RetornarPerimetro());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Challenge6Ejer1/Rectanguloo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge6Ejer1
+{
+    partial class Rectangulo
+    {
+        public int RetornarSuperficie()
+        {
+            return lado1 * lado2;
+        }
+
+        public int RetornarPerimetro()
+        {
+            return lado1 * 2 + lado2 * 2;
+        }
+    }
+}
